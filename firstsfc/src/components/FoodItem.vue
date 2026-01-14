1ff3492486fab7ed469df10bb64d068cb10b86b2
Rubin <rslukongo@student.apc.edu.ph>
2026-01-14 14:42:10 +0800

    i added apple

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Berries and Apple',
+      message: 'I Love berries and apple'
+    }
+  }
+};
+</script>
+
+<style></style>
