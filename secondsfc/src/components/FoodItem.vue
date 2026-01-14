a5680afb8745a0e66c08ccf31e881d2893087236
Rubin <rslukongo@student.apc.edu.ph>
2026-01-14 06:09:47 +0000

    second project

---
+++ secondsfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Berries',
+      message: 'I Love Berries'
+    }
+  }
+};
+</script>
+
+<style></style>
